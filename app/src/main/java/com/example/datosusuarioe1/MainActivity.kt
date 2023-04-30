498208e909a38d4f8f872588df22de72804cf1e3
DLAP18 <dlap-16@hotmail.com>
2023-04-30 00:03:38 -0600

    Afinando detalles de diseño en el formulario

--- app/src/main/java/com/example/datosusuarioe1/MainActivity.kt
+++ app/src/main/java/com/example/datosusuarioe1/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.datosusuarioe1
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.datosusuarioe1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +18,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.carreras,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.scarreras.adapter = adapter
+        }
+
+        //binding.scarreras.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            //override fun onItemSelected(
+                //parent: AdapterView
+            //)
+        //}
     }
+
+    fun clickBoton(view: View){
+        if (binding.scarreras.selectedItemPosition == 0){
+            Toast.makeText(this,
+            "Por favor, seleccione una opción",
+            Toast.LENGTH_SHORT).show()
+        } else{
+            Toast.makeText(this,
+            "La opción seleccionada es el número ${binding.scarreras.selectedItemPosition}",
+            Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isValidEmail(mail: CharSequence) =
+        (!TextUtils.isEmpty(mail) && Patterns.EMAIL_ADDRESS.matcher(mail).matches())
 }
